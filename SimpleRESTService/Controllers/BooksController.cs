44fb74bed047df3205c43a5d5ec701fefcb45f77
pc <pc>
2021-10-09 17:44:29 +0200

    Added http status codes to the API crude methods

--- SimpleRESTService/Controllers/BooksController.cs
+++ SimpleRESTService/Controllers/BooksController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Threading.Tasks;
 using BookClassLibrary;
+using Microsoft.AspNetCore.Http;
 using SimpleRESTService.Manager;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,38 +19,81 @@ namespace SimpleRESTService.Controllers
         private readonly BooksManager _manager = new BooksManager();
 
         // GET: api/<BooksController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet]
-        public IEnumerable<Book> Get()
+        public ActionResult<IEnumerable<Book>> Get()
         {
-            return _manager.GetAll(null);
+            IEnumerable<Book> books = _manager.GetAll(null);
+
+            if (books == null || !books.Any())
+            {
+                return NotFound("No Books found");
+            }
+
+            return Ok(books);
+
         }
 
         // GET api/<BooksController>/BookName
-        [HttpGet("{id}")]
-        public IEnumerable<Book> Get(string substring)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{substring}")]
+        public ActionResult<IEnumerable<Book>> Get(string substring)
         {
-            return _manager.GetAll(substring);
+            IEnumerable<Book> books = _manager.GetAll(substring);
+
+            if (books == null || !books.Any())
+            {
+                return NotFound("No Books found with this name");
+            }
+
+            return Ok(books);
         }
 
+
         // POST api/<BooksController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
-        public Book Post([FromBody] Book value)
+        public ActionResult<EventBookmark> Post([FromBody] Book value)
         {
-            return _manager.Add(value);
+            Book book = _manager.Add(value);
+            if (book == null)
+            {
+                return BadRequest("Data was null");
+            }
+
+            return Ok(book);
         }
 
         // PUT api/<BooksController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut("{isbn}")]
-        public Book Put(string isbn, [FromBody] Book newBook)
+        public ActionResult<Book> Put(string isbn, [FromBody] Book newBook)
         {
-            return _manager.Update(isbn, newBook);
+            Book book = _manager.Update(isbn, newBook);
+            if (book == null)
+            {
+                return BadRequest("Data was null");
+            }
+
+            return Ok(book);
         }
 
         // DELETE api/<BooksController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpDelete("{isbn}")]
-        public Book Delete(string isbn)
+        public ActionResult<Book> Delete(string isbn)
         {
-            return _manager.Delete(isbn);
+            Book book = _manager.Delete(isbn);
+            if (book == null)
+            {
+                return BadRequest("Data was null");
+            }
+            return Ok(book);
         }
     }
 }
