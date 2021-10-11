0a3d2d9422bc1db6a467970bd1f123b6a8606058
pc <pc>
2021-10-12 01:37:16 +0200

    Add more books

--- SimpleRESTService/Manager/BooksManager.cs
+++ SimpleRESTService/Manager/BooksManager.cs
@@ -10,7 +10,9 @@ namespace SimpleRESTService.Manager
     {
         private static readonly List<Book> bookList = new List<Book>()
         {
-            new Book(){Author = "Pedro", Isbn = "01010101", PageNumber = 800, Title = "Nice"}
+            new Book(){Author = "Pedro", Isbn = "01010101", PageNumber = 800, Title = "Nice"},
+            new Book(){Author = "John", Isbn = "91919191", PageNumber = 800, Title = "IT Essentials"},
+            new Book(){Author = "Jane", Isbn = "30303003", PageNumber = 800, Title = "I have no imagination"}
         };
 
         
